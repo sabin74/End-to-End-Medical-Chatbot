6dc0ee6578a8720722ec5adce3efb514573e943d
Sabin Lamsal <sabinlamsal74@gmail.com>
2026-08-04 09:34:13 +0000

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author = "Sabin Lamsal",
+    author_email = "sabinlamsal74@gmail.com",
+    packages = find_packages(),
+    install_requires=[]
+)
