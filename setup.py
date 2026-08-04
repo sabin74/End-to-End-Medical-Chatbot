57286d4ac2cf4fcaac801e55c79249f101e8dc2d
sabin74 <sabinlamsal74@gmail.com>
2026-08-04 21:02:52 +0545

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="medical_chatbot",
     version="0.1.0",
-    author = "Sabin Lamsal",
-    author_email = "sabinlamsal74@gmail.com",
-    packages = find_packages(),
+    author="Boktiar Ahmed Bappy",
+    author_email="entbappy73@gmail.com",
+    packages=find_packages(),
     install_requires=[]
 )
